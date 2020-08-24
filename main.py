b3fd1d1fa122244f14b946f3f413930c01a21e24
Ljw <ljws93@naver.com>
2020-08-24 11:39:05 +0900

    Star_work_200824
    1. Create unit
    
    2. Check Number of unit
    
    3. Attack command and Die

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-class Unit_Comman_prats:
+import random
+
+class Unit_Comman_prats:    #일반 유닛 특성 저장
     def __init__(self, name, hp, damage):
         self.name = name
         self.hp = hp
         self.damage = damage
-class Our_Unit:
+
+class Our_Unit: #아군 유닛 생성
     def __init__(self, name, hp, damage):
         Unit_Comman_prats.__init__(self, name, hp, damage)
         print("{}유닛이 생성되었습니다.".format(self.name))
@@ -11,6 +14,9 @@ class Our_Unit:
 
     def attack(self, location):
         print("{}이 {}시 방향으로 공격합니다. 공격력[{}]".format(self.name, location, self.damage))
+
+    def hp(self):
+        return self.hp
 
 class Enemy_Unit:
     def __init__(self, name, hp, damage):
@@ -22,6 +28,9 @@ i = 0   # 커맨드 자동 반복
 j = 0   # 게임 스타트 버튼
 marine_num = 0   # 아군 마린 수
 tank_num = 0    # 아군 탱크 수
+enemy_location = 1
+marine_all = []
+tank_all = []
 
 def create_unit():
     global marine_num
@@ -41,11 +50,47 @@ def create_unit():
 #     while globals(i) == 0:
 
 def check_unit():
-    unit_name = input("확인할 병력을 입력하세요: ")
+    unit_name = input("확인할 병력을 입력하세요: (help 입력시 생산 가능 유닛 나옴)")
     if unit_name == "marine":
         print("현재 마린의 수는 {}입니다".format(marine_num))
     elif unit_name == "tank":
         print("현재 탱크의 수는 {}입니다.".format(tank_num))
+    elif unit_name == "help":
+        print("현재 생산 가능한 유닛은 marine, tank 입니다")
+
+def attack_command():   # 유닛별로 조건을 만들어 공격하고 데미지를 입게 한다
+    location = input("공격할 위치(시간)을 입력하세요 : (예 : 1)")
+    if marine_num > 0:  # 마린이 1기라도 있을 시
+        print("마린 {}기가 {}시 방향으로 공격합니다".format(marine_num, location))
+        if int(location) == int(enemy_location):  # 공격 위치에 적이 있을 시(적 위치는 랜덤)
+            get_damaged("marine", marine_num)   # 유닛 이름, 유닛 갯수
+
+    if tank_num > 0:
+        print("탱크 {}기가 {}시 방향으로 공격합니다".format(tank_num, location))
+        if int(location) == int(enemy_location):  # 공격 위치에 적이 있을 시(적 위치는 랜덤)
+            get_damaged("tank", tank_num)  # 유닛 이름, 유닛 갯수
+
+
+def get_damaged(unit_name, unit_num):
+    unit_count = 0  # 유닛 카운터 초기화
+    global marine_num
+    global tank_num
+    while unit_count < unit_num:
+        damage = random.randint(1, 200)
+        print("{} 1기가 {}의 데미지를 입었습니다.".format(unit_name, damage))
+        if damage >= 40 and unit_name == "marine":
+            print("[{} 1기가 죽었습니다]".format(unit_name))
+            marine_num -= 1
+        elif damage >= 150 and unit_name =="tank":
+            print("[{} 1기가 죽엇습니다]".format(unit_name))
+            tank_num -= 1
+
+
+        unit_count += 1
+
+
+def checkEnemy():
+    print("적의 위치는 {}시입니다.".format(enemy_location))
 
 
 while i == 0:
@@ -57,9 +102,14 @@ while i == 0:
     elif command == "help":
         print("도움말 목록")
         print("quit : 프로그램 종료")
+        print("start : 게임 시작")
+        print("create : 유닛 생산")
+        print("check : 생성된 유닛 확인")
+        print("attack : 모든 유닛 공격 명령")
 
     elif command == "start" and j == 0:
         print("게임을 시작합니다")
+        enemy_location = range(1, 12)
         j = 1
 
     elif command == "create":
@@ -67,6 +117,11 @@ while i == 0:
 
     elif command == "check":
         check_unit()
+
+    elif command == "attack":
+        attack_command()
+    elif command == "enemy location":
+        checkEnemy()
 
     else:
         print("커맨드를 다시 입력하세요")
